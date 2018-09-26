840dfc2658270de32852b95d31c0b2465bcf9df7
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-09-26 19:27:40 +0200

    Add doc dep, test bash 5.0

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 BINS="bin/loop"
 LIBS="lib/loop.sh"
-DEPS="gitlab.com/shellm/core"
+DEPS="gitlab.com/shellm/doc"
 # BASH_COMPLETIONS="cmp/loop.completion.bash"
 # ZSH_COMPLETIONS="cmp/loop.completion.zsh"
